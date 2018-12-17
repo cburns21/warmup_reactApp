29c889563fd0cabd5a5caafb52a99c1494fd853d
caroline burns <carolineburns@carolines-MacBook.local>
2018-12-17 12:03:48 -0700

    week 2 of code along

---
+++ app.js
@@ -0,0 +1,4 @@
+import React, { Component}
+from 'react'
+import xmasList from './components/list.js'
+import items from './data.js'
---
+++ data.js
@@ -0,0 +1,9 @@
+const items = [
+    { item: 'real estate',
+      size: '5000 sq ft'
+    },
+    { item: 'diamonds',
+      size: 'large'
+    }
+]
+export default items 
